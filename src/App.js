6165b6bbafc233f4423d92a76dc80451feec1891
Arad Bivas <41302200+aradbivas@users.noreply.github.com>
2022-10-02 12:27:55 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './Components/Home'
 import AboutMe from './Components/AboutMe'
 import Projects from './Components/Projects'
